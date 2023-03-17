28e4fdd0ad8fbdb223e128652b03a981332fb77a
yildirimsinop <norvecoslo57@>
2023-03-17 02:33:37 +0100

    Skol

---
+++ src/skolProje/Skol.java
@@ -0,0 +1,14 @@
+
+package SkolProje;
+
+public class Skol {
+
+    static final String okulIsmi = "Istanbul Koleji";
+    static final String adres = "Beyoglu / Istanbul";
+    static final String telefon = "0216 436 36 96";
+
+    private Skol () {
+
+
+    }
+}

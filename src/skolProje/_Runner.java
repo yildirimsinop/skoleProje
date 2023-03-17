923f64352ca515d0bac086368c627c5afc624e06
yildirimsinop <norvecoslo57@>
2023-03-17 02:30:16 +0100

    runner

---
+++ src/skolProje/_Runner.java
@@ -0,0 +1,12 @@
+package SkolProje;
+
+public class _Runner {
+    public static void main(String[] args) throws InterruptedException {
+
+        Larer.feykOgretmenEkle();
+        SkolProje.Lager.anaMenu();
+
+    }
+
+
+}
